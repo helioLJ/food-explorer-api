1639cb74a5d5e35c6c6fd15c73bf2d6891adf00d
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-04-28 09:59:26 -0300

    UsersController Update

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -23,6 +23,34 @@ class UsersController {
 
     response.status(201).json({ message: "Usuário criado com sucesso!" })
   }
+
+  async update(request, response) {
+    const { name, email, old_password, new_password } = request.body
+    const { id } = request.params
+
+
+    if (!name || !email) {
+      throw new AppError("Os campos não podem estar vazios.")
+    }
+
+    const user = await knex("users").where("id", id).first();
+
+    if(!user) {
+      throw new AppError("Usuário não encontrado.", 404)
+    }
+
+    if (email !== user.email) {
+      const userWithEmail = await knex("users").where("email", email).first();
+      if (userWithEmail) {
+        throw new AppError("O email informado já está em uso por outro usuário.");
+      }
+    }
+
+    await knex("users").where("id", id).update(user);
+
+
+    return response.status(200).json({ message: "Usuário editado com sucesso!" })
+  }
 }
 
 module.exports = UsersController
